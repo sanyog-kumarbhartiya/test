376679bbbb8c2e8a2a3bda966ca702c944676cfc
sanyog-kumarbhartiya <sanyogkumarbhartiya@gmail.com>
2024-03-26 14:50:20 +0530

    dfcf

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
-const { defineConfig } = require("cypress");
+const path = require('path');
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output bundle file name
   },
-});
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this rule to files ending in .js
+        exclude: /node_modules/, // Don't apply to files residing in node_modules
+        use: {
+          loader: 'babel-loader', // Use babel-loader for transpiling
+          options: {
+            presets: ['@babel/preset-env'], // Use @babel/preset-env for configuring babel
+          },
+        },
+      },
+      // Add more rules for handling different file types (e.g., CSS, images)
+    ],
+  },
+  // Add plugins, resolve options, and other configuration as needed
+};
